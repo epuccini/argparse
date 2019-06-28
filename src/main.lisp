0599fbd0ce8b582b9dba2a52b2b7b1aed4cabd35
Edward.Puccini <Edward.Puccini@partner.bmw.de>
2019-06-28 14:48:33 +0200

    Refactor

--- src/main.lisp
+++ src/main.lisp
@@ -1,9 +1,9 @@
 ;;; -----------------------------------------------------
 ;;; main
 ;;; -----------------------------------------------------
-;;; @File:     argparse/src/main.lisp
-;;; @Date:     09:18:24 of Tuesday, 6/18/2019 (GMT+1)
-;;; @Author:   Edward Puccini
+;;; File:     argparse/src/main.lisp
+;;; Date:     09:18:24 of Tuesday, 6/18/2019 (GMT+1)
+;;; Author:   Edward Puccini
 ;;; -----------------------------------------------------
 
 (require 'argparse)
